4d8d9cd48f6266e630f74122626841aa58f5f52a
Shawn Nunoo <shawnnunoo@gmail.com>
2019-07-12 15:49:06 -0700

    Restful Tasks Continued

--- public/src/app/app.component.ts
+++ public/src/app/app.component.ts
@@ -7,6 +7,17 @@ import { HttpService } from './http.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'public';
-  constructor(private _httpService: HttpService){}
+  title = 'Restful Tasks API';
+  tasks = ["Learn Angular - Understand services", "Manipulate the DOM - User the 'for of' loop", "Bind Events - Parentheses indicate events"];
+
+
+  constructor(private _httpService: HttpService) { }
+  getTasks() {
+    let taskObservable = this._httpService.getTasks();
+    taskObservable.subscribe(data => {
+      console.log("Got our tasks!", data);
+      this.tasks = data['tasks']
+    }
+    )
+  }
 }
--- public/src/app/http.service.ts
+++ public/src/app/http.service.ts
@@ -9,10 +9,7 @@ export class HttpService {
   constructor(private _http: HttpClient) {
     this.getTasks();
   }
-  getTasks() {
-    // our http response is an Observable, store it in a variable
-    let tempObservable = this._http.get('/tasks');
-    // subscribe to the Observable and provide the code we would like to do with our data from the response
-    tempObservable.subscribe(data => console.log("Got our tasks!", data));
+  getTasks(){
+    return this._http.get('/tasks')
   }
 }
