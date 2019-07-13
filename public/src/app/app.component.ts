acd34084df168a7b287f12911675336a3a5f96ab
Shawn Nunoo <shawnnunoo@gmail.com>
2019-07-12 22:51:39 -0700

    Restful Tasks API Interactive

--- public/src/app/app.component.ts
+++ public/src/app/app.component.ts
@@ -9,7 +9,8 @@ import { HttpService } from './http.service';
 export class AppComponent {
   title = 'Restful Tasks API';
   tasks = [];
-  tasksid = {};
+  is_tasks = false;
+  task_info = {};
 
 
   constructor(private _httpService: HttpService) {
@@ -20,24 +21,25 @@ export class AppComponent {
     taskObservable.subscribe(data => {
       console.log("Got our tasks!", data);
       console.log(data)
-      this.tasks = data.data;
+      this.tasks = data.data
     })
   }
 
-  getTasksById() {
+  getTasksById(task) {
+    console.log('showing one task')
+    var tasks_id = task._id
     let taskObservable = this._httpService.getTasks();
     taskObservable.subscribe(data => {
-      console.log("Got our tasks!", data);
+      console.log("Got one tasks!", data);
       console.log(data)
-      this.tasksid = data.data;
+      this.task_info = data.data
+      this.is_tasks = true
     })
   }
+
   onButtonClick(){
-    // console.log(`Click is working`);
+    console.log(`Click is working`);
     this.getTasks()
   }
-  onButtonClick2(){
-    console.log(`Click is working`);
-    this.getTasksById()
-  }
+ 
 }
